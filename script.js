8250424443faeead83d60deb825d87c53be2b376
dmitryure <dmitryure@gmail.com>
2019-08-11 18:36:02 +0300

    distances added

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ const settings = {
 
 const sketch = () => {
   let dimensions = settings.dimensions
-  let rows = 3
-  let columns = 4
+
+  const createObjects = (numberOfObjects) => {
+    let arrOfObjects = []
+    let multiplier = Math.sqrt(numberOfObjects)
+    const x = dimensions[0]
+    const y = dimensions[1]
+    let distanceX = x / multiplier
+    let distanceY = y / multiplier
+    for (let i = 1; i <= numberOfObjects; i++) {
+      for (let j = 1; j <= Math.ceil(multiplier); j++) {
+        let obj = {}
+        console.log(dimensions[1]/multiplier*i)
+        console.log(multiplier)
+        console.log(dimensions[1]/i * multiplier)
+        obj.y = y - distanceY * j
+        obj.x = x - distanceX * j
+        arrOfObjects.push(obj)
+      }
+      return arrOfObjects
+    }
+  }
+
+  console.log(createObjects(10))
 
 
-  console.log(dimensions)
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
