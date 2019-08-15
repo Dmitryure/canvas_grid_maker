7ac85938940a0edc5a8d03da1fc31faf95a61507
dmitryure <dmitryure@gmail.com>
2019-08-15 21:25:14 +0300

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvasSketch = require('canvas-sketch');
 const utils = require('canvas-sketch-util')
 
 const settings = {
-	dimensions: [2048, 2048]
+	dimensions: [4096, 2048]
 };
 
 const sketch = () => {
@@ -17,12 +17,12 @@ const sketch = () => {
 		let distanceX = x / multiplier
 		let distanceY = y / multiplier
 		let counter = 1
-		for (let i = 1; i <= elementsPerSide; i++) {
-			for (let j = 1; j <= elementsPerSide; j++) {
+		for (let i = 1; i < elementsPerSide; i++) {
+			for (let j = 1; j < elementsPerSide; j++) {
 				let obj = {}
 				//for large numbers drop -1
-				obj.y = y - distanceY * i / (Math.sqrt(elementsPerSide) -1)
-				obj.x = x - distanceX * j / (Math.sqrt(elementsPerSide) -1)
+				obj.y = distanceY + y - distanceY * i / (Math.sqrt(elementsPerSide) -1)
+				obj.x = distanceX + x - distanceX * j / (Math.sqrt(elementsPerSide) -1)
 				obj.id = counter
 				arrOfObjects.push(obj)
 				counter++
@@ -31,7 +31,7 @@ const sketch = () => {
 		return arrOfObjects
 	}
 
-	let numOfObjects = 42
+	let numOfObjects = 42 
 
 	return ({context, width, height}) => {
 		console.log(createObjects(numOfObjects))
